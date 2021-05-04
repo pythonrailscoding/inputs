5a2924fa823605bb4aa438070bc10539115e07be
Umesh Purohit <umeshpurohit1975@gmail.com>
2021-05-04 15:59:44 +0530

    updated Repositry

---
+++ example of taking input in c++.cpp
@@ -0,0 +1,26 @@
+/* Include these headers for modern C++ IDE's like Dev-C++
+#include <iostream>
+using namespace std;
+int main()
+*/
+
+//Use This header for Turbo-C++
+#include <iostream.h>
+#include <conio.h>
+//Using any editor, invoke this  method
+int sum(int a, int b){
+	int c;
+	c = a + b;
+	return c;
+}
+int main(){
+	int a, b
+	cout <<  "Enter a number";
+	cin >> a;
+	cout << "Enter another number";
+	cin >> b;
+	cout << "The sum of " << a << " and " << b << " is " << sum(a, b);
+	//Include this statement for Turbo-C++
+	getch();
+	return 0;
+}
---
+++ example of taking input in python.py
@@ -0,0 +1,3 @@
+a = int(input("Enter a number"))
+b = int(input("Enter another number"))
+print("The sum of ", a, " and ", b, " is ", (a+b))
